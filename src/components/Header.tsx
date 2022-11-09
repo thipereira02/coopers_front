752055b5fb741a758bf478970f2d4230e0ea4353
Thiago <thiago.spereira02@gmail.com>
2022-11-09 15:32:58 -0300

    adding header

---
+++ src/components/Header.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+
+import logo from "../assets/Fill 1.jpg";
+
+export default function Header() {
+	return (
+		<Content>
+			<Name>
+				<img src={logo} alt="logo" />
+				<h1>coopers</h1>
+			</Name>
+			<SignIn>
+                entrar
+			</SignIn>
+		</Content>
+	);
+}
+
+const Content = styled.div`
+    width: 100vw;
+    height: 6.25rem;
+    padding: 0 2rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const Name = styled.div`
+    display: flex;
+    align-items: center;
+
+    img{
+        height: 1.8rem;
+        margin-right: 0.4rem;
+    }
+
+    h1{
+        font-size: 1.6rem;
+        font-weight: 600;
+    }
+`;
+
+const SignIn = styled.button`
+    width: 5rem;
+    height: 2.5rem;
+    border-radius: 0.5rem;
+    border: none;
+    background-color: #000;
+    color: #FFF;
+    font-family: 'Poppins', sans-serif;
+    font-size: 0.875rem;
+    font-weight: 600;
+    cursor: pointer;
+
+    @media(min-width: 768px){
+        width: 7.5rem;
+    }
+`;
