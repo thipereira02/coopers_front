5c304152692b7b35cf5c5f44a9782969cdfa5b3a
Thiago <thiago.spereira02@gmail.com>
2022-11-14 13:54:18 -0300

    adding sign in modal

--- src/ModalPopup/BaseModalWrapper.tsx
+++ src/ModalPopup/BaseModalWrapper.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Modal from "./Modal";
-import { DesktopModalContainer, Header } from "./ModalPopupStyle";
+
+import { DesktopModalContainer, Title, Form } from "./ModalPopupStyle";
+import image from "../assets/image 1.jpg";
 
 interface BaseModalWrapperProps {
     isModalVisible: boolean;
@@ -13,7 +15,21 @@ const BaseModalWrapper: React.FC<BaseModalWrapperProps> = ({onBackdropClick, isM
 	return(
 		<Modal onBackdropClick={onBackdropClick}>
 			<DesktopModalContainer>
-				<Header>Modal Info</Header>
+				<p onClick={onBackdropClick}>close</p>
+				<Title>
+					<img src={image} alt="image" />
+					<div>
+						<h1>Sign in</h1>
+						<h2>to access your list</h2>
+					</div>
+				</Title>
+				<Form>
+					<label htmlFor="name">User:</label>
+					<input type="name"/>
+					<label htmlFor="password">Password:</label>
+					<input type="password" />
+					<button>Sign in</button>
+				</Form>
 			</DesktopModalContainer>
 		</Modal>
 	); 
--- src/ModalPopup/ModalPopupStyle.tsx
+++ src/ModalPopup/ModalPopupStyle.tsx
@@ -1,24 +1,132 @@
 import styled from "styled-components";
 
 const ModalContainer = styled.div`
-    background-color: #000;
+    background-color: #FFF;
     display: flex;
     flex-direction: column;
     align-items: center;
-    position: relative;
+    margin: 0 auto;
+    width: 90%;
+
+    @media (min-width: 768px) {
+        width: 100%;
+    }
 `;
 
 export const DesktopModalContainer = styled(ModalContainer)`
-    border-radius: 0.5rem;
-    box-shadow: 0 0 2rem rgba(0, 0, 0, 0.5);
-    padding: 2.5rem;
-    width: 28rem;
+    padding: 1.4375rem;
+
+    p{
+        font-size: 1rem;
+        font-weight: 700;
+        cursor: pointer;
+        align-self: flex-end;
+        margin-bottom: 0.75rem;
+    }
+
+    @media (min-width: 768px) {
+        p{
+            font-size: 1.25rem;
+        }
+    }
 `;
 
-export const Header = styled.h3`
-    color: #FFF;
-    font-size: 1.5rem;
-    font-weight: 600;
-    margin: 0.3rem 0 0.625rem;
-    text-align: center;
+export const Title = styled.div`
+    display: flex;
+    margin: 0 auto;
+
+    img{
+        height: 8rem;
+    }
+
+    div{
+        display: flex;
+        flex-direction: column;
+        align-items: start;
+        justify-content: center;
+
+        h1{
+            color: #000;
+            font-size: 2.5rem;
+            font-weight: 700;
+            margin: 0.3rem 0 0.625rem;
+            text-align: center;
+        }
+    
+        h2{
+            color: #4AC959;
+            font-size: 1.5rem;
+            font-weight: 400;
+        }
+    }
+
+    @media (min-width: 768px) {
+        img{
+            height: 10rem;
+        }
+
+        div{
+            h1{
+                font-size: 3rem;
+            }
+
+            h2{
+                font-size: 2rem;
+            }
+        }
+    }
+`;
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    width: 60%;
+    margin-top: 1rem;
+
+    label{
+        font-size: 1.2rem;
+        font-weight: 600;
+        margin-top: 0.5rem;
+        margin-bottom: 0.2rem;
+        font-size: 1rem;
+    }
+
+    input{
+        border: 1px solid #959595;
+        border-radius: 10px;
+        height: 2.5rem;
+        padding: 0 0.5rem;
+    }
+
+    button{
+        background-color: #4AC959;
+        border: none;
+        color: #FFF;
+        font-size: 1rem;
+        font-weight: 700;
+        height: 3rem;
+        margin-top: 2rem;
+        margin-bottom: 0.5625rem;
+        width: 80%;
+        align-self: center;
+    }
+
+    @media (min-width: 768px) {
+        width: 55%;
+
+        label{
+            font-size: 1.5rem;
+            margin-top: 1rem;
+        }
+
+        input{
+            height: 3rem;
+            font-size: 1.2rem;
+        }
+
+        button{
+            font-size: 1.5rem;
+            height: 3.5rem;
+        }
+    }
 `;
